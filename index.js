b28a055354e98b6773cddfbf55d3ce92f12fd3cd
sky <flahertysky12@gmail.com>
2021-05-26 18:15:08 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+ var customerName = 'bob'
+ const leastFavoriteCustomer = 'maybe bob'
+
+ function upperCaseCustomerName() {    
+    customerName = customerName.toUpperCase();
+
+ }
+
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+}
+    
+
+function overwriteBestCustomer() {
+    bestCustomer = leastFavoriteCustomer;
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'Tony';
+}
+
+
